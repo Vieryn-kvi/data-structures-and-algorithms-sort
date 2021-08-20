6b1a557f04025b94d99069bc90bc22baccd2355b
kvi <kvi@netup.ru>
2021-08-20 17:54:31 +0300

    Refactor

--- src/main/java/ru/vieryn/Main.java
+++ src/main/java/ru/vieryn/Main.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Main {
     private static void bubbleSort(int[] arr) {
-        for (int i = arr.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j+1]) {
-                    swap(arr, j, j + 1);
+        for (int lastUnsortedIndex = arr.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (arr[i] > arr[i+1]) {
+                    swap(arr, i, i + 1);
                 }
             }
         }
