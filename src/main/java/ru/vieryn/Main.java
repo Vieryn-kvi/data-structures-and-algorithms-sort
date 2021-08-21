609f8285a28ea0acece262a80b6538f3f128d48d
kvi <kvi@netup.ru>
2021-08-21 20:14:24 +0300

    Add selection sort implementation

--- src/main/java/ru/vieryn/Main.java
+++ src/main/java/ru/vieryn/Main.java
@@ -12,11 +12,30 @@ public class Main {
             }
         }
     }
+    private static void selectionSort(int[] arr) {
+        if (arr == null || arr.length == 0) return;
+        for (int lastUnsortedIndex = arr.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int maxIndex = 0;
+            int maxValue = arr[maxIndex];
+            for (int i = 1; i < lastUnsortedIndex; i++) {
+                int curValue = arr[i];
+                if (curValue > maxValue) {
+                    maxValue = curValue;
+                    maxIndex = i;
+                }
+            }
+            if (maxValue > arr[lastUnsortedIndex])
+                swap(arr, maxIndex, lastUnsortedIndex);
+        }
+    }
     public static void main(String[] args) {
         int[] sourceArray = {3, 15, -15, 35, -22, 55, 0};
         int[] bubbleArray = Arrays.copyOf(sourceArray, sourceArray.length);
+        int[] selectionArray = Arrays.copyOf(sourceArray, sourceArray.length);
         bubbleSort(bubbleArray);
+        selectionSort(selectionArray);
         System.out.println(Arrays.toString(bubbleArray));
+        System.out.println(Arrays.toString(selectionArray));
     }
     private static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
