3d5387e5d35e0e6d18c6e9901ff78755379ffd06
kvi <kvi@netup.ru>
2021-08-22 13:15:24 +0300

    Add insertion sort algorithm implementation

--- src/main/java/ru/vieryn/Main.java
+++ src/main/java/ru/vieryn/Main.java
@@ -28,14 +28,30 @@ public class Main {
                 swap(arr, maxIndex, lastUnsortedIndex);
         }
     }
+    private static  void insertionSort(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
+            int current = arr[firstUnsortedIndex];
+            int i;
+            for (i = firstUnsortedIndex; i > 0; i--) {
+                int toMove = arr[i - 1];
+                if (toMove <= current) break;
+                arr[i] = toMove;
+            }
+            arr[i] = current;
+        }
+    }
     public static void main(String[] args) {
         int[] sourceArray = {3, 15, -15, 35, -22, 55, 0};
         int[] bubbleArray = Arrays.copyOf(sourceArray, sourceArray.length);
         int[] selectionArray = Arrays.copyOf(sourceArray, sourceArray.length);
+        int[] insertionArray = Arrays.copyOf(sourceArray, sourceArray.length);
         bubbleSort(bubbleArray);
         selectionSort(selectionArray);
+        insertionSort(insertionArray);
         System.out.println(Arrays.toString(bubbleArray));
         System.out.println(Arrays.toString(selectionArray));
+        System.out.println(Arrays.toString(insertionArray));
     }
     private static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
